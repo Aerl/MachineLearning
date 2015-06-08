8fa305b97fc06c5d264852a958bed9f55110e3d7
gollabaer <bjoern.golla@gmx.de>
2015-06-08 11:45:05 +0200

    *labeled unlabeled data

--- Neuer Ordner/src/test.java
+++ Neuer Ordner/src/test.java
@@ -6,24 +6,35 @@ import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.unsupervised.attribute.Add;
 
 public class test {
  
     public static void main(String[] args) throws Exception{
     	// read data from file
-    	DataSource source = new DataSource("C:/Users/Bjrn/Desktop/SS2015/MachineLearning/train.csv");
-    	DataSource sourcetest = new DataSource("C:/Users/Bjrn/Desktop/SS2015/MachineLearning/test.csv");
+    	DataSource source = new DataSource("C:/train.csv");
+    	DataSource sourcetest = new DataSource("C:/test.csv");
     	Instances datatest = sourcetest.getDataSet();
     	Instances data = source.getDataSet();
     	//set class attribute
     	data.setClassIndex(data.numAttributes()-1);
     	
+    	// 2. numeric attribute
+        Add filter = new Add();
+        filter.setAttributeIndex("last");
+        filter.setAttributeName("target1");
+        filter.setInputFormat(datatest);
+        datatest = filter.useFilter(datatest, filter);
+    	for ( int i = 0; i < datatest.numInstances(); i++){
+    		datatest.instance(i).setValue(datatest.numAttributes() - 1, -1);
+    	}
+        datatest.setClassIndex(datatest.numAttributes()-1);
     	KStar k = new KStar();
     	k.buildClassifier(data);
     	System.out.println("loaded");
     	for(int i = 0; i < datatest.numInstances();i++)
         {
-
+    		
             double score = k.classifyInstance(datatest.instance(i));
             double[] vv= k.distributionForInstance(datatest.instance(i));
             System.out.println("class");
